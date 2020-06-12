7a37e7151b2b388c439ff6d4d1449eb32311a512
apollo2mond <apollo2mond@users.noreply.github.com>
2020-06-12 21:38:14 +0200

    added unroll option

--- megaavr/cores/arduino/wiring_shift.c
+++ megaavr/cores/arduino/wiring_shift.c
@@ -22,28 +22,91 @@
 
 #include <Arduino.h>
 
+#define _SHIFTIN_LSB_BIT() \
+	do { \
+		digitalWrite(clockPin, HIGH); \
+		val = (val>>1) | (digitalRead(dataPin) << 7); \
+		digitalWrite(clockPin, LOW); \
+	} while (0)
+
+#define _SHIFTIN_MSB_BIT() \
+	do { \
+		digitalWrite(clockPin, HIGH); \
+		val = (val<<1) | digitalRead(dataPin); \
+		digitalWrite(clockPin, LOW); \
+	} while (0)
+
 uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
 	uint8_t val = 0;
-
-	for (uint8_t = 0; i != 8; ++i) {
+#   ifndef _SHIFT_RX_UNROLL_LOOP
+	for (uint8_t i = 0; i != 8; i++) {
 		digitalWrite(clockPin, HIGH);
+		
 		if (bitOrder == LSBFIRST)
-			val = (val>>1) | (digitalRead(dataPin)<<7);
+			val = (val>>1) | (digitalRead(dataPin) << 7);
 		else
 			val = (val<<1) | digitalRead(dataPin);
+		
 		digitalWrite(clockPin, LOW);
 	}
+#   else
+	if (bitOrder == LSBFIRST) {
+		_SHIFTIN_LSB_BIT(); _SHIFTIN_LSB_BIT();
+		_SHIFTIN_LSB_BIT(); _SHIFTIN_LSB_BIT();
+		_SHIFTIN_LSB_BIT(); _SHIFTIN_LSB_BIT();
+		_SHIFTIN_LSB_BIT(); _SHIFTIN_LSB_BIT();	
+	}
+	else {
+		_SHIFTIN_MSB_BIT(); _SHIFTIN_MSB_BIT();
+		_SHIFTIN_MSB_BIT(); _SHIFTIN_MSB_BIT();
+		_SHIFTIN_MSB_BIT(); _SHIFTIN_MSB_BIT();
+		_SHIFTIN_MSB_BIT(); _SHIFTIN_MSB_BIT();
+	}
+#   endif
+
 	return val;
 }
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val) {
-	for (uint8_t i = 0; i != 8; i++)  {
-		if (bitOrder == LSBFIRST)
-			digitalWrite(dataPin, val & 0x01), val >>= 1;		
-		else
-			digitalWrite(dataPin, !!(val & 0x80)), val <<= 1;
+#define _SHIFTOUT_LSB_BIT() \
+	do { \
+		digitalWrite(dataPin, val & 0x01); \
+		val >>= 1; \
+		digitalWrite(clockPin, HIGH); \
+		digitalWrite(clockPin, LOW); \
+	} while (0)
 
+#define _SHIFTOUT_MSB_BIT() \
+	do { \
+		digitalWrite(dataPin, !!(val & 0x80)); \
+		val <<= 1; \
+		digitalWrite(clockPin, HIGH); \
+		digitalWrite(clockPin, LOW); \
+	} while (0)
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val) {
+#   ifndef _SHIFT_TX_UNROLL_LOOP
+	for (uint8_t i = 0; i != 8; i++) {
+
+		if (bitOrder == LSBFIRST)
+			digitalWrite(dataPin, val & 0x01), val >>= 1;
+		else	
+			digitalWrite(dataPin, !!(val & 0x80)), val <<= 1;
+			
 		digitalWrite(clockPin, HIGH);
 		digitalWrite(clockPin, LOW);
 	}
+#   else
+	if (bitOrder == LSBFIRST) {
+		_SHIFTOUT_LSB_BIT(); _SHIFTOUT_LSB_BIT();
+		_SHIFTOUT_LSB_BIT(); _SHIFTOUT_LSB_BIT();
+		_SHIFTOUT_LSB_BIT(); _SHIFTOUT_LSB_BIT();
+		_SHIFTOUT_LSB_BIT(); _SHIFTOUT_LSB_BIT();
+	}
+	else {
+		_SHIFTOUT_MSB_BIT(); _SHIFTOUT_MSB_BIT();
+		_SHIFTOUT_MSB_BIT(); _SHIFTOUT_MSB_BIT();
+		_SHIFTOUT_MSB_BIT(); _SHIFTOUT_MSB_BIT();
+		_SHIFTOUT_MSB_BIT(); _SHIFTOUT_MSB_BIT();
+	}
+#   endif
 }
